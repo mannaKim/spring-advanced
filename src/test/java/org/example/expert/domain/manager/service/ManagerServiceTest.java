240694e18f9d54c82f0cc2a6c1fb59641ca0917f
hanna <hannah.k0925@gmail.com>
2025-02-25 21:05:05 +0900

    test(lv3): 테스트 코드 수정

--- src/test/java/org/example/expert/domain/manager/service/ManagerServiceTest.java
+++ src/test/java/org/example/expert/domain/manager/service/ManagerServiceTest.java
@@ -19,11 +19,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -39,14 +41,16 @@ class ManagerServiceTest {
     private ManagerService managerService;
 
     @Test
-    public void manager_목록_조회_시_Todo가_없다면_NPE_에러를_던진다() {
+    public void manager_목록_조회_시_Todo가_없다면_InvalidRequestException을_던진다() {
         // given
         long todoId = 1L;
         given(todoRepository.findById(todoId)).willReturn(Optional.empty());
 
         // when & then
-        InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> managerService.getManagers(todoId));
-        assertEquals("Manager not found", exception.getMessage());
+        InvalidRequestException exception = assertThrows(InvalidRequestException.class, () ->
+                managerService.getManagers(todoId)
+        );
+        assertEquals("Todo not found", exception.getMessage());
     }
 
     @Test
